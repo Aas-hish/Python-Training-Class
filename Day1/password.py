6dee55bbf6ade77ba25ab6d1a2dbe415460755f1
Aashish <148946422+Aas-hish@users.noreply.github.com>
2025-04-21 14:37:53 +0530

    final

--- Day1/password.py
+++ Day1/password.py
@@ -1,5 +1,24 @@
 
 passs = input("Enter passwaord:")
+dg=0
+up=0
+sp=0
+sm=0
 
-for i in str(passs):
-    
+if (len(passs)>7):
+    for i in passs:
+        if(i.isupper()):
+            up=up+1
+        elif(i.islower()):
+            sm= sm+1
+        elif(i.isdigit()):
+            dg=dg+1
+        else:
+            sp=sp+1
+            
+    if(up>0 and sm>0 and dg>0 and sp>0 ):
+        print("Strong Password")
+    else:
+        print("Weak Password")
+else:
+    print("Need to alteast 8 character")
