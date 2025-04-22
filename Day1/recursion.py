e1abe8dc7a4b465d4a07fbd7a1bfe0d6fe5f8406
Aashish <148946422+Aas-hish@users.noreply.github.com>
2025-04-22 11:05:45 +0530

    final

--- Day1/recursion.py
+++ Day1/recursion.py
@@ -38,12 +38,22 @@
 # main()
 
 
-def abc(n):
-    if n==1:
-        return 2
-    b = abc(n-1)
-    print("hai",end=" ")
-    return b + 1
+# def abc(n):
+#     if n==1:
+#         return 2
+#     b = abc(n-1)
+#     print("hai",end=" ")
+#     return b + 1
 
-print(abc(5))
+# print(abc(5))
+
+
+def prime(n):
+    if n==1:
+        return []
+    for i in range(2,n+1):
+        if n%i==0:
+            return [i] + prime(n//i)
         
+a = int(input("Enter a number: "))       
+print(prime(a))
---
+++ Day2/string_compression.py
@@ -0,0 +1,16 @@
+def com_string(s):
+    compressesd = ""
+    count = 1
+    for i in range(len(s)-1):
+        if (s[i]==s[i+1]):
+            count += 1
+        else:
+            compressesd = compressesd+ s[i] + str(count)
+            count = 1
+            
+    compressesd = compressesd + s[-1] + str(count)
+    print(compressesd)
+            
+            
+s= input("Enter a string: ")
+com_string(s)
