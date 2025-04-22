01d27c3ad620c1597cdabef14e5aef62888ef6d6
Aashish <148946422+Aas-hish@users.noreply.github.com>
2025-04-22 16:13:20 +0530

    final

--- Day2/digital_clock.py
+++ Day2/digital_clock.py
@@ -1,11 +1,22 @@
-from datetime import datetime
-import pytz
 import time
 import os
-a = pytz.timezone('Asia/Kolkata')
 
+h=3 
+m=3
+s=0
 while True:
-    b = datetime.now(a)
-    print(b.strftime("%H:%M:%S"))
+
+    print("{:02d}:{:02d}:{:02d}".format(h,m,s))
     time.sleep(1)
+    s=s+1
+    if(s==60):
+        s=0
+        m=m+1
+    elif(m==60):
+        m=0
+        h=h+1
+    elif(h==12):
+        s=0
+        m=0
+        h=0
     os.system("clear")
