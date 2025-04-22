a0a3acf865326186008614c15e631b76eb274794
Aashish <148946422+Aas-hish@users.noreply.github.com>
2025-04-22 12:26:02 +0530

    final

--- Day2/flames.py
+++ Day2/flames.py
@@ -9,8 +9,8 @@ for i in range(len(l1)):
         if l1[i] == l2[j]:
             l1[i] = '2'
             l2[j] = '2'
-print(l1)
-print(l2)
+# print(l1)
+# print(l2)
 x = sum(1 for i in l1 if i != '2')
 y = sum(1 for j in l2 if j != '2')
 count = x + y
