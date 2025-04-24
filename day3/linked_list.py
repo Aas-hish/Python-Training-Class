f978a98def6146687c4dbc3af970a2ca1d97d896
Aashish <148946422+Aas-hish@users.noreply.github.com>
2025-04-24 09:28:32 +0530

    final

--- day3/linked_list.py
+++ day3/linked_list.py
@@ -28,10 +28,35 @@ class Linkedlist:
             res.append(temp.data)
             temp=temp.next
         print(" ---> ".join(map(str,res)))
-            
+    
+    def insertAtpos(self, data, pos):
+        if(not self.head):
+            print("List is empty")
+            return
+        if(pos==1):
+            new_node = Node(data)
+            new_node.next= self.head
+            self.head = new_node
+            return
+        
+        temp = self.head
+        for i in range(1,pos-1):
+            if not temp:
+                print("Position out of range")
+                return
+            temp = temp.next
+
+        if not temp:
+            print("Position out of range")
+            return
+        new_node = Node(data)
+        new_node.next = temp.next
+        temp.next = new_node
+        
 
 l= Linkedlist()
 l.insert(100)
 l.insert(300)
 l.insert(900)
+l.insertAtpos(500,4)
 l.display()
