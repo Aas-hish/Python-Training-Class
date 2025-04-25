0e5568fdc7fab11eaf7c4bb21edadc1c1837bbbc
Aashish <148946422+Aas-hish@users.noreply.github.com>
2025-04-25 10:39:03 +0530

    final

--- day5/grid_problem.py
+++ day5/grid_problem.py
@@ -1,7 +1,36 @@
 import random as r
 
-rows, cols = 4,5
-g = [[r.randint(1, 20) for _ in range(cols)] for _ in range(rows)]
 
-for row in g:
-    print(row)
+def generate_grid(rows,cols):
+    if rows<3 or cols<3:
+        print("Not possible")
+        return
+    
+    while True:
+        g = [[r.randint(1, 99) for _ in range(cols)] for _ in range(rows)]
+        outer_sum = 0
+        inner_sum = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+                    outer_sum += g[i][j]
+                else:
+                    inner_sum += g[i][j]
+                    
+        if outer_sum==inner_sum:
+            print("\nBalanced Grid")
+            for row in g:
+                print(row)
+            print("\nOuter Sum:", outer_sum)
+            print("Inner Sum:", inner_sum)
+            break
+
+rows= int(input("Enter number of row: "))
+cols= int(input("Enter number of cols: "))
+generate_grid(rows,cols)
+
+    
+
+
+    
