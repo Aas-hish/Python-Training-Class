b339a7cbb25ea942196b491b0a0dd11d87365a1d
Aashish <148946422+Aas-hish@users.noreply.github.com>
2025-05-02 11:52:26 +0530

    final

---
+++ file/read.py
@@ -0,0 +1,4 @@
+with open("Aashish.txt","r") as file:
+    line = file.readlines()
+    for i in line:
+        print(i)
---
+++ file/write.py
@@ -0,0 +1,4 @@
+file = open("Aashish.csv","w")
+file.write("Hi How are you? I am Aashish Sah ")
+file.close()
+
